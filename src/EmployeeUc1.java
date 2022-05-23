209c193f37ec585b5f9b4ebc39f2412586be9c18
Pooja <pooja.katkr@gmail.com>
2022-05-23 21:37:55 +0530

    hello

---
+++ src/EmployeeUc1.java
@@ -0,0 +1,17 @@
+
+public class EmployeeUc1 {
+
+		public static void main(String[] args) {
+			
+			int IS_FULL_TIME = 5;
+			
+			double empCheck = Math.floor(Math.random() * 10)% 2;
+			
+			if (empCheck == IS_FULL_TIME)
+			   System.out.println("Employee is Present");
+			else
+		       System.out.println("Employee is Abscent");		
+			}
+	}
+
+
